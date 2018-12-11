1abbb007709c1b9ee58dda61734461a00a476018
Huy Hung <icetea7578@gmail.com>
2018-12-11 21:41:13 +0700

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+##Install guide
+
+Install package
+```
+pip install -r requirements.txt 
+```
+
+##Usage
+Run
+```
+python3 server.py
+
+```
+Send http get request to `localhost:2685/` to get json respone
+
+
+
---
+++ server.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+app = Flask(__name__)
+api = Api(app)
+
+timeA = 0
+prev_timeA = 0
+used_timeA = 0
+sttA = 0
+
+timeB = 0
+prev_timeB = 0
+used_timeB = 0
+sttB = 0 
+
+timeC = 0
+prev_timeC = 0
+used_timeC = 0
+sttC = 0
+
+class ParkingServer(Resource):
+    def handle(self, req):
+        js = json.loads(req)['data']
+        """ Global variable """
+        global timeA, timeB, timeC, prev_timeA, prev_timeB, prev_timeC,used_timeA, used_timeB, used_timeC, sttA, sttB, sttC
+        """ Parse data from request """
+        timeA = int(js[0]['toggle_time'])
+        timeB = int(js[1]['toggle_time'])
+        timeC = int(js[2]['toggle_time'])
+        print( timeA, '-', timeB, '-', timeC)
+        """ Handling time value"""
+        if timeA != 0:
+            if prev_timeA != 0:
+                used_timeA = timeA - prev_timeA
+                prev_timeA = 0
+                sttA = 0
+            else:
+                prev_timeA = 0
+                sttA = 1
+
+        if timeB != 0:
+            if prev_timeB != 0:
+                used_timeB = timeB - prev_timeB
+                prev_timeB = 0
+                sttB = 0
+            else:
+                prev_timeB = 0
+                sttB = 1
+
+        if timeC != 0:
+            if prev_timeC != 0:
+                used_timeC = timeC - prev_timeC
+                prev_timeC = 0
+                sttC = 0
+            else:
+                prev_timeC = 0
+                sttC = 1
+    
+    def post(self):
+        req = request.data
+        print(req)
+        self.handle(req)
+        return "Update success"
+
+    def get(self):
+        global sttA, sttB, sttC
+        response = {"data": ["","",""]}
+        response["data"][0] = sttA
+        response["data"][1] = sttB
+        response["data"][2] = sttC
+        print(response)
+        return response
+    
+api.add_resource(ParkingServer, '/')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 2685, debug = True)
+
+
+
+
+
